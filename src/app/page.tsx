81001724cd03fbfb013440367b390979c308804d
Arnav <atzgg132@gmail.com>
2024-10-20 21:27:22 +0530

    clients section finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
+import Clients from "../components/Clients";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Clients />
+    </>
+  );
 }
