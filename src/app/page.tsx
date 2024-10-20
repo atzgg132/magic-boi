024da9859e2515cf28a9533f61abf240ae7c0ab4
Arnav <atzgg132@gmail.com>
2024-10-20 23:13:13 +0530

    added landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import AutoScrollingTestimonialCarousel from "@/components/Feedback";
 
+import Landing from "../components/Landing";
 import Photos from "../components/Photos";
 import Clients from "../components/Clients";
 
 export default function Home() {
   return (
     <>
+      <Landing />
       <AutoScrollingTestimonialCarousel />
       <Photos />
       <Clients />
